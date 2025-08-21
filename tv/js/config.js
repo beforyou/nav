25f60146fc74f696cc7243980243ed122f55ecd2
beforyou <50078025+beforyou@users.noreply.github.com>
2025-08-21 22:49:13 +0800

    Update config.js

--- tv/js/config.js
+++ tv/js/config.js
@@ -16,58 +16,23 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-     heimuer: {
+    sm: {
         api: 'https://img.smdyw.top',
-        name: '黑木耳',
-        detail: 'https://img.smdyw.top'
+        name: '神马',
     },
-    tyyszy: {
-        api: 'https://tyyszy.com',
-        name: '天涯资源',
+    bf: {
+        api: 'https://bfzyapi.com',
+        name: '暴风',
+    },    
+    md: {
+        api: 'https://www.mdzyapi.com',
+        name: '墨斗',
     },
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
         adult: true
     },
-    zy360: {
-        api: 'https://360zy.com',
-        name: '360资源',
-    },
-    wolong: {
-        api: 'https://wolongzyw.com',
-        name: '卧龙资源',
-    },
-    cjhw: {
-        api: 'https://cjhwba.com',
-        name: '新华为',
-    },
-    jisu: {
-        api: 'https://jszyapi.com',
-        name: '极速资源',
-        detail: 'https://jszyapi.com'
-    },
-    dbzy: {
-        api: 'https://dbzy.com',
-        name: '豆瓣资源',
-    },
-    bfzy: {
-        api: 'https://bfzyapi.com',
-        name: '暴风资源',
-    },
-    mozhua: {
-        api: 'https://mozhuazy.com',
-        name: '魔爪资源',
-    },
-    mdzy: {
-        api: 'https://www.mdzyapi.com',
-        name: '魔都资源',
-    },
-    ruyi: {
-        api: 'https://cj.rycjapi.com',
-        name: '如意资源',
-    },
-    
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
